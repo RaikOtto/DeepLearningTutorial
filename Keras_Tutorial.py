52ad3a4be43b0031d0c6a7f1b780848c8ff25fd0
RaikOtto <fubioinf@gmail.com>
2018-01-23 12:41:20 +0100

    Create Keras_Tutorial.py

---
+++ Keras_Tutorial.py
@@ -0,0 +1,112 @@
+import sys; print('Python %s on %s' % (sys.version, sys.platform))
+
+# Create first network with Keras
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load pima indians dataset
+dataset = numpy.loadtxt("/home/ottoraik/Downloads/pima-indians-diabetes.data", delimiter=",",  )
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(1, init='uniform', activation='sigmoid'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X, Y, epochs=150, batch_size=10,  verbose=2)
+# calculate predictions
+predictions = model.predict(X)
+# round predictions
+rounded = [round(x[0]) for x in predictions]
+print(rounded)
+
+########
+
+model = Sequential()
+model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(1, init='uniform', activation='sigmoid'))
+# 2. compile the network
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# 3. fit the network
+history = model.fit(X, Y, epochs=100, batch_size=10)
+# 4. evaluate the network
+loss, accuracy = model.evaluate(X, Y)
+print("\nLoss: %.2f, Accuracy: %.2f%%" % (loss, accuracy*100))
+# 5. make predictions
+probabilities = model.predict(X)
+predictions = [float(round(x)) for x in probabilities]
+accuracy = numpy.mean(predictions == Y)
+print("Prediction Accuracy: %.2f%%" % (accuracy*100))
+
+###
+
+
+# 1. define the network
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# 2. compile the network
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# 3. fit the network
+history = model.fit(X, Y, epochs=100, batch_size=10)
+# 4. evaluate the network
+loss, accuracy = model.evaluate(X, Y)
+print("\nLoss: %.2f, Accuracy: %.2f%%" % (loss, accuracy*100))
+# 5. make predictions
+probabilities = model.predict(X)
+predictions = [float(round(x)) for x in probabilities]
+accuracy = numpy.mean(predictions == Y)
+print("Prediction Accuracy: %.2f%%" % (accuracy*100))
+
+### https://www.learnopencv.com/deep-learning-using-keras-the-basics/
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.datasets import boston_housing
+
+(X_train, Y_train), (X_test, Y_test) = boston_housing.load_data()
+
+nFeatures = X_train.shape[1]
+
+model = Sequential()
+model.add(Dense(1, input_shape=(nFeatures,), activation='linear'))
+
+model.compile(optimizer='rmsprop', loss='mse', metrics=['mse', 'mae'])
+
+model.fit(X_train, Y_train, batch_size=4, epochs=1000)
+
+model.summary()
+
+model.evaluate(X_test, Y_test, verbose=True)
+
+Y_pred = model.predict(X_test)
+
+print Y_test[:5]
+print Y_pred[:5, 0]
+
+### https://keras.io/
+
+from keras.models import Sequential
+
+model = Sequential()
+from keras.layers import Dense
+
+model.add(Dense(units=64, activation='relu', input_dim=100))
+model.add(Dense(units=10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',optimizer='sgd',metrics=['accuracy'])
+
+model.compile(loss='categorical_crossentropy',optimizer=s.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+model.fit(x_train, y_train, epochs=5, batch_size=32)
+
+model.train_on_batch(x_batch, y_batch)
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+classes = model.predict(x_test, batch_size=128)
