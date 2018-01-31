da787ca2d8bfa6b7140e0ab8eb49d84e52a95d98
RaikOtto <fubioinf@gmail.com>
2018-01-31 13:08:12 +0100

    Update Tutorial_2_Cifar_10.py

--- Tutorial_2_Cifar_10.py
+++ Tutorial_2_Cifar_10.py
@@ -1,61 +1,109 @@
-# Link to the tutorial: http://heatmapping.org/wifs2017/training.pdf
+'''Train a simple deep CNN on the CIFAR10 small images dataset.
+It gets to 75% validation accuracy in 25 epochs, and 79% after 50 epochs.
+(it's still underfitting at that point, though).
+'''
 
-# YOU CAN GET THE REQUIRED DATA HERE
-# https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+from __future__ import print_function
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import os
 
-import numpy,scipy,scipy.io
-Xtrain = []
-Ttrain = []
-for batch in [1,2,3,4,5]:
-    D = scipy.io.loadmat('cifar-10-batches-mat/data_batch_%d'%batch) # CHANGE PATH
-    Xtrain += [(D['data']/127.5-1.0).reshape([-1,3,32,32]).transpose([0,2,3,1])]
-    Ttrain += [(D['labels'][:,numpy.newaxis] == numpy.arange(10)).reshape([-1,1,1,10])*1.0]
-    Xtrain = numpy.concatenate(Xtrain,axis=0)
-    Ttrain = numpy.concatenate(Ttrain,axis=0)
+batch_size = 32
+num_classes = 10
+epochs = 100
+data_augmentation = True
+num_predictions = 20
+save_dir = os.path.join(os.getcwd(), 'saved_models')
+model_name = 'keras_cifar10_trained_model.h5'
 
+# The data, shuffled and split between train and test sets:
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 
-D = scipy.io.loadmat(’cifar-10-batches-mat/test_batch’)
-Xtest = (D[’data’][:500]/127.5-1.0).reshape([-1,3,32,32]).transpose([0,2,3,1])
-Ttest = (D[’labels’][:500][:,numpy.newaxis] == numpy.arange(10)).reshape([-1,1,1,10])
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
-%matplotlib inline
-import utils
-utils.images(Xtest[:50])
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-nn = md.Network([
-md.Convolution(’cnn/c1-5x5x3x20’,write=True),md.ReLU(),md.Pooling(),
-md.Convolution(’cnn/c2-5x5x20x50’,write=True),md.ReLU(),md.Pooling(),
-md.Convolution(’cnn/c3-4x4x50x200’,write=True),md.ReLU(),md.Pooling(),
-md.Convolution(’cnn/c4-1x1x200x10’,write=True),
-])
-nn.layers[-1].W*=0
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-def trainingstep():
-  # 1. select random mini-batch
-  R = numpy.random.permutation(len(Xtrain))[:25]
-  x,t = Xtrain[R],Ttrain[R]
-  # 2. forward pass
-  y = nn.forward(x)
-  # 3. error gradient
-  dy = numpy.exp(y) / (numpy.exp(y).sum(axis=1)[:,numpy.newaxis] + 1) - t
-  # 4. backward pass
-  nn.gradprop(dy)
-  2
-  # 5. updating the parameters with some learning rate
-  nn.update(0.001)
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
 
-def teststep(i):
-  # 1. store the network
-  nn.dump()
-  # 2. compute prediction labels
-  pred=numpy.argmax(nn.forward(Xtest),axis=-1)
-  # 3. extract target labels
-  targ=numpy.argmax(Ttest,axis=-1)
-  # 4. check how often they match (this gives a measure of predictive accuracy)
-  print(’%6d %.3f’%(i,(pred==targ).mean()))
+# initiate RMSprop optimizer
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
 
-import numpy,os
-cpt = [10,20,40,100,200,400,1000,2000,4000,6000,8000,10000,12000,14000,16000,18000,20000]
-for i in range(1,20001):
-  trainingstep()
-if i in cpt: teststep(i)
+# Let's train the model using RMSprop
+model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_data=(x_test, y_test),
+              shuffle=True)
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+    # Compute quantities required for feature-wise normalization
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(x_train)
+
+    # Fit the model on the batches generated by datagen.flow().
+    model.fit_generator(datagen.flow(x_train, y_train,
+                                     batch_size=batch_size),
+                        epochs=epochs,
+                        validation_data=(x_test, y_test),
+                        workers=4)
+
+# Save model and weights
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+model_path = os.path.join(save_dir, model_name)
+model.save(model_path)
+print('Saved trained model at %s ' % model_path)
+
+# Score trained model.
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
