ff015a88566d9ed143252824b8ff3d29dcbd85fc
Jannes Münchmeyer <munchmej@informatik.hu-berlin.de>
2018-02-05 14:59:32 +0100

    Tutorial 1 Update

--- Tutorial_1_Medical_data.py
+++ Tutorial_1_Medical_data.py
@@ -4,16 +4,17 @@ import sys; print('Python %s on %s' % (sys.version, sys.platform))
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
-import urllib2
+import urllib
+from sklearn.metrics import confusion_matrix
 
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 
 # load pima indians dataset
-response = urllib2.urlopen('https://github.com/RaikOtto/DeepLearningTutorial/raw/master/pima-indians-diabetes.csv')
-raw_data = response.read()
-dataset = raw_data.split("\n")
+response = urllib.request.urlopen('https://github.com/RaikOtto/DeepLearningTutorial/raw/master/pima-indians-diabetes.csv')
+raw_data = response.read().decode()
+dataset = numpy.stack([numpy.fromstring(x, sep=",") for x in raw_data.split('\n') if x])
 #dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",",  )
 # split into input (X) and output (Y) variables
 X = dataset[:,0:8]
@@ -27,48 +28,15 @@ model.add(Dense(1, init='uniform', activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
 model.fit(X, Y, epochs=150, batch_size=10,  verbose=2)
+# Evaluate the network
+loss, accuracy = model.evaluate(X, Y)
+print("\nLoss: %.2f, Accuracy: %.2f%%" % (loss, accuracy*100))
 # calculate predictions
-predictions = model.predict(X)
-# round predictions
-rounded = [round(x[0]) for x in predictions]
-print(rounded)
-
-########
-
-model = Sequential()
-model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='sigmoid'))
-# 2. compile the network
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# 3. fit the network
-history = model.fit(X, Y, epochs=100, batch_size=10)
-# 4. evaluate the network
-loss, accuracy = model.evaluate(X, Y)
-print("\nLoss: %.2f, Accuracy: %.2f%%" % (loss, accuracy*100))
-# 5. make predictions
 probabilities = model.predict(X)
-predictions = [float(round(x)) for x in probabilities]
-accuracy = numpy.mean(predictions == Y)
-print("Prediction Accuracy: %.2f%%" % (accuracy*100))
+predictions = [float(x>0.5) for x in probabilities]
 
-###
-
-
-# 1. define the network
-model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-# 2. compile the network
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# 3. fit the network
-history = model.fit(X, Y, epochs=100, batch_size=10)
-# 4. evaluate the network
-loss, accuracy = model.evaluate(X, Y)
-print("\nLoss: %.2f, Accuracy: %.2f%%" % (loss, accuracy*100))
-# 5. make predictions
-probabilities = model.predict(X)
-predictions = [float(round(x)) for x in probabilities]
-accuracy = numpy.mean(predictions == Y)
-print("Prediction Accuracy: %.2f%%" % (accuracy*100))
-
+cm = confusion_matrix(Y, predictions)
+print ("True positives:  %.0f" % cm[1,1])
+print ("False positives: %.0f" % cm[0,1])
+print ("True negatives:  %.0f" % cm[0,0])
+print ("False negatives: %.0f" % cm[1,0])
