57f94d79b843d5f904ba1ff173427e5cedaed308
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2018-02-08 11:51:29 +0100

    make script fit for running on server

--- Tutorial_1_Medical_data.py
+++ Tutorial_1_Medical_data.py
@@ -1,54 +1,50 @@
 import sys; print('Python %s on %s' % (sys.version, sys.platform))
 
 # make sure to connect via 'xterm -ls -xrm 'XTerm*selectToClipboard: true'&
-
-# Create first network with Keras
-from keras.models import Sequential
-from keras.layers import Dense
-import numpy
-import urllib2
-from sklearn.metrics import confusion_matrix
-from os.path import expanduser
+import os
+# set the environment variable
+os.environ["KERAS_BACKEND"] = "tensorflow"
+# make sure we were successful
+print(os.environ.get("KERAS_BACKEND"))
 
 # set preloading
-
 import tensorflow as tf
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
+# Create first network with Keras
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+from sklearn.metrics import confusion_matrix
+from os.path import expanduser
+
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 
-# load pima indians dataset and store to disk
+# load pima dataset
 url = 'https://github.com/RaikOtto/DeepLearningTutorial/raw/master/pima-indians-diabetes.csv'
-response = urllib2.urlopen( url )
-data = response.read()
-
-home = expanduser("~")
-filename = home + "/pima-indians-diabetes.csv"
-
-with open( filename, 'w'  ) as write_handle:
-    write_handle.write(data)
-
-# load pima indians dataset
-dataset = numpy.loadtxt(filename, delimiter=",")
-dataset
+response = urllib.request.urlopen( url )
+dataset = numpy.loadtxt(response, delimiter=",")
     
 # split into input (X) and output (Y) variables
 X = dataset[:,0:8]
 Y = dataset[:,8]
+
 # create model
 model = Sequential()
 model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
 model.add(Dense(8, init='uniform', activation='relu'))
 model.add(Dense(1, init='uniform', activation='sigmoid'))
+
 # Compile model
 model.compile(
     loss='binary_crossentropy',
     optimizer='adam',
     metrics=['accuracy'])
+
 # Fit the model
 history = model.fit(
     X,
@@ -67,19 +63,20 @@ predictions = [float(x>0.5) for x in probabilities]
 ## visualizations WORK ONLY VIA 'ssh -XC' and 'xterm python' or locally on a laptop
 
 # loss function
-import matplotlib.pyplot as plt
+import gnuplotlib as gpl
 
 loss = history.history['loss']
 
 epochs = range(1, len(loss) + 1)
 
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.title('Training loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.show()
+gpl.plot(epochs, loss,
+	_with='points', 
+	title='Training loss', 
+	legend='Training loss', 
+	xlabel='Epochs',
+	ylabel='Loss',
+	unset='grid',
+	terminal='dumb 40,80')
 
 # accuracy
 
@@ -87,18 +84,30 @@ plt.clf()   # clear figure
 
 acc = history.history['acc']
 
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.title('Training accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.show()
+gpl.plot(epochs, acc,
+	_with='points', 
+	title='Training accuracy', 
+	legend='Training acc', 
+	xlabel='Epochs',
+	ylabel='Accuracy',
+	unset='grid',
+	terminal='dumb 40,80')
 
 #
 
 cm = confusion_matrix(Y, predictions)
-print ("True positives:  %.0f" % cm[1,1])
-print ("False positives: %.0f" % cm[0,1])
-print ("True negatives:  %.0f" % cm[0,0])
-print ("False negatives: %.0f" % cm[1,0])
+tp = float(cm[1,1])
+fp = float(cm[0,1])
+tn = float(cm[0,0])
+fn = float(cm[1,0])
+print ("True positives:  %.0f" % tp)
+print ("False positives: %.0f" % fp)
+print ("True negatives:  %.0f" % tn)
+print ("False negatives: %.0f" % fn)
+
+prec = tp/(tp+fp)
+rec = tp/(tp+fn)
+f1 = (2*prec*rec)/(prec+rec)
+print ("Precision: %.3f" % prec)
+print ("Recall: %.3f" % rec)
+print ("F1: %.3f" % f1)
