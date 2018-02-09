62f48328b8b020e7ab948c52318d81077e06d5b0
RaikOtto <fubioinf@gmail.com>
2018-02-09 11:19:46 +0100

    Update Day_2_Setup_jupyter.sh

--- Day_2_Setup_jupyter.sh
+++ Day_2_Setup_jupyter.sh
@@ -1,17 +1,3 @@
-# connect to server XX is your specific login
-ssh mdc-XX@farm.parallel-computing.pro
-# enter password
-
-# install jupyter
-python -m pip install jupyter --user # perhaps already installed
-
-# set the password
-~/.local/bin/jupyter notebook password # tensorflow1
-
-# start the server
-nohup ~/.local/bin/jupyter notebook --no-browser
-### LEAVE THE SERVER AND START LOCAL TERMINAL
-
 # connect to server notebook LOCAL
 ssh -nNT -L 9999:localhost:8888 mdc-XX@farm.parallel-computing.pro # enter password
 
