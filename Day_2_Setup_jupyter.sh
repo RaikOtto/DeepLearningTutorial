b570449a7a7ea12f78887569b0e6f8baf620c6f9
RaikOtto <fubioinf@gmail.com>
2018-02-09 11:01:09 +0100

    Update and rename Setup_jupyter.sh to Day_2_Setup_jupyter.sh

--- Day_2_Setup_jupyter.sh
+++ Day_2_Setup_jupyter.sh
@@ -1,4 +1,4 @@
-# connect to server
+# connect to server XX is your specific login
 ssh mdc-XX@farm.parallel-computing.pro
 # enter password
 
@@ -12,3 +12,10 @@ python -m pip install jupyter --user # perhaps already installed
 nohup ~/.local/bin/jupyter notebook --no-browser
 ### LEAVE THE SERVER AND START LOCAL TERMINAL
 
+# connect to server notebook LOCAL
+ssh -nNT -L 9999:localhost:8888 mdc-XX@farm.parallel-computing.pro # enter password
+
+# connect to notebook GO TO BROWSER
+localhost:9999 # enter password
+
+# finished, use jupyter notebook on NVIDIA GPU server
